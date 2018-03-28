58c8653b693df240b95d2ed6cf4596dc44510643
Alex Popkov <poalrom@yandex.ru>
2018-03-28 15:02:33 +0300

    Fix bug wint separated webpack configs and styleguide

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,9 @@
+const webpackConfig = require('./webpack.prod.js');
+
 module.exports = {
   title: 'Style Guide',
   showUsage: true,
+  webpackConfig,
   sortProps: props => props.sort((a, b) => {
     if (a.required && !b.required) { return -1; }
     if (!a.required && b.required) { return 1; }
